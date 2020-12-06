09ad53c3ad9f34164712183474771283ed05145f
paul028 <paulvincentnonat@gmail.com>
2020-12-06 14:36:34 +0800

    fast

--- MLP_withPCA+SF_dataset_modified.py
+++ MLP_withPCA+SF_dataset_modified.py
@@ -74,11 +74,16 @@ def generate_dataset(components,random_state,sf_n,oor_value):
           delete_item.append(w)
           print("Row",w,"Less than 3 Gateways")
           
-    for w in range(len(delete_item)):
-        print("deleting row",delete_item[w]," Total Rows to delete: ",len(delete_item)," Remaining Rows: ",(size-len(delete_item)))
-        x=np.delete(x,delete_item[w])
-        y=np.delete(y,delete_item[w])
-        SF=np.delete(SF,delete_item[w])
+#    for w in range(len(delete_item)):
+#        print("deleting row",delete_item[w]," Total Rows to delete: ",len(delete_item)," Remaining Rows: ",(size-len(delete_item)))
+#        x=np.delete(x,delete_item[w])
+#        y=np.delete(y,delete_item[w])
+#        SF=np.delete(SF,delete_item[w])
+    
+    print(" Total Rows to delete: ",len(delete_item)," Remaining Rows: ",(size-len(delete_item)))
+    x=np.delete(x,delete_item)
+    y=np.delete(y,delete_item)
+    SF=np.delete(SF,delete_item)
     
     if oor_value==0:
         final_x = get_powed_distance_np(x,-200)
