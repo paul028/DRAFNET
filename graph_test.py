243bdd5bfd7383ce39a480fa62d642da12a71621
paul028 <paulvincentnonat@gmail.com>
2021-02-07 11:41:55 +0800

    KNN

--- graph_test.py
+++ graph_test.py
@@ -12,28 +12,39 @@ import csv
 import matplotlib.pyplot as plt
 from random import randrange, uniform
 import random
+import numpy
+
+
+def residuals(p, data):  # Residuals function needed by kmpfit
+   x, y = data           # Data arrays is a tuple given by programmer
+   a, b = p              # Parameters which are adjusted by kmpfit
+   return (y-(a+b*x))
+
 
 #dataset 1 -128dB without SF, with SF
 x = np.array([1,2,3,4,5,6,7])
 #trial_name1=["MLP.csv","MLP+PCA=44.csv","MLP+PCA=40.csv","MLP+PCA=10.csv","MLP+PCA=7.csv","MLP+PCA=5.csv","MLP+PCA=3.csv"]
+#trial_name1=["ext+PCA=3.csv","ext+PCA=5.csv","ext+PCA=7.csv","ext+PCA=10.csv","ext+PCA=40.csv","ext+PCA=44.csv","ext.csv"]
+trial_name1=["KNN+PCA=3.csv","KNN+PCA=5.csv","KNN+PCA=7.csv","KNN+PCA=10.csv","KNN+PCA=40.csv","KNN+PCA=44.csv","KNN.csv"]
 
-trial_name1=["MLP+PCA=3.csv","MLP+PCA=5.csv","MLP+PCA=7.csv","MLP+PCA=10.csv","MLP+PCA=40.csv","MLP.csv","MLP+PCA=44.csv"]
-
-trial_name2=["MLP+PCA=3+SF.csv","MLP+PCA=5+SF.csv","MLP+PCA=7+SF.csv","MLP+PCA=10+SF.csv","MLP+PCA=40+SF.csv","MLP+PCA=44+SF.csv","MLP+SF.csv"]
+trial_name2=["KNN+PCA=3+SF.csv","KNN+PCA=5+SF.csv","KNN+PCA=7+SF.csv","KNN+PCA=10+SF.csv","KNN+PCA=40+SF.csv","KNN+PCA=44+SF.csv","KNN+SF.csv"]
 #trial_name2=["MLP+SF.csv","MLP+PCA=44+SF.csv","MLP+PCA=40+SF.csv","MLP+PCA=10+SF.csv","MLP+PCA=7+SF.csv","MLP+PCA=5+SF.csv","MLP+PCA=3+SF.csv"]
+#trial_name2=["ext+SF.csv","ext+PCA=44+SF.csv","ext+PCA=40+SF.csv","ext+PCA=10+SF.csv","ext+PCA=7+SF.csv","ext+PCA=5+SF.csv","ext+PCA=3+SF.csv"]
 
 #dataset 2 Out of Range Dependent on SF
 #trial_name3=["MLP_SFD.csv","MLP+PCA=44_SFD.csv","MLP+PCA=40_SFD.csv","MLP+PCA=10_SFD.csv","MLP+PCA=7_SFD.csv","MLP+PCA=5_SFD.csv","MLP+PCA=3_SFD.csv"]
-trial_name3=["MLP+PCA=3_SFD.csv","MLP+PCA=5_SFD.csv","MLP+PCA=7_SFD.csv","MLP+PCA=10_SFD.csv","MLP+PCA=40_SFD.csv","MLP+PCA=44_SFD.csv","MLP_SFD.csv"]
+#trial_name3=["ext_SFD.csv","ext+PCA=44_SFD.csv","ext+PCA=40_SFD.csv","ext+PCA=10_SFD.csv","ext+PCA=7_SFD.csv","ext+PCA=5_SFD.csv","ext+PCA=3_SFD.csv"]
+trial_name3=["KNN+PCA=3_SFD.csv","KNN+PCA=5_SFD.csv","KNN+PCA=7_SFD.csv","KNN+PCA=10_SFD.csv","KNN+PCA=40_SFD.csv","KNN+PCA=44_SFD.csv","KNN_SFD.csv"]
 
 #trial_name4=["MLP+SF_SFD.csv","MLP+PCA=44+SF_SFD.csv","MLP+PCA=40+SF_SFD.csv","MLP+PCA=10+SF_SFD.csv","MLP+PCA=7+SF_SFD.csv","MLP+PCA=5+SF_SFD.csv","MLP+PCA=3+SF_SFD.csv"]
-trial_name4=["MLP+PCA=3+SF_SFD.csv","MLP+PCA=5+SF_SFD.csv","MLP+PCA=7+SF_SFD.csv","MLP+PCA=10+SF_SFD.csv","MLP+PCA=40+SF_SFD.csv","MLP+PCA=44+SF_SFD.csv","MLP+SF_SFD.csv"]
+#trial_name3=["ext_SFD.csv","ext+PCA=44_SFD.csv","ext+PCA=40_SFD.csv","ext+PCA=10_SFD.csv","ext+PCA=7_SFD.csv","ext+PCA=5_SFD.csv","ext+PCA=3_SFD.csv"]
+trial_name4=["KNN+PCA=3+SF_SFD.csv","KNN+PCA=5+SF_SFD.csv","KNN+PCA=7+SF_SFD.csv","KNN+PCA=10+SF_SFD.csv","KNN+PCA=40+SF_SFD.csv","KNN+PCA=44+SF_SFD.csv","KNN+SF_SFD.csv"]
 trial_name=[trial_name1,trial_name2,trial_name3,trial_name4]
 df_list=list()
 for w in range(len(trial_name)):
     df =list()
     for q in range(len(trial_name[w])):
-        df.append(pd.read_csv("original_data/"+trial_name[w][q],header=None).iloc[1:].values.astype('float64'))#.values.astype('float64')
+        df.append(pd.read_csv("KNN_modified/"+trial_name[w][q],header=None).iloc[1:].values.astype('float64'))#.values.astype('float64')
     df_list.append(df) #[without,with][trial]
 
 
@@ -47,7 +58,7 @@ for w in range(len(trial_name)):
             temp.append(df_list[w][z][q][1])
             
         distance_error.append(temp)
-    distance_error_list.append(distance_error)
+    distance_error_list.append(distance_errerror)
 
 #df=df.iloc[1:].values.astype('float64')
 errors_mean_list=list()
@@ -84,7 +95,8 @@ ax.set_xticks(x)
 ax.set_xticklabels(labels)
 ax.set_ylabel("Mean Positioning Error(m)")
 ax.set_xlabel("Number of Receiving Base Station")
-ax.set_title("Performance Comparison in Atwerp LoRaWAN Dataset")
-ax.legend(['7L_MLP+OOR1','7L_MLP+OOR1+SF','7L_MLP+OOR2','7L_MLP+OOR2+SF'])
-plt.savefig("model_performance_mean.png",dpi=200,bbox_inches = 'tight')
+ax.set_title("Performance Comparison in Reduced Atwerp LoRaWAN Dataset")
+ax.legend(['KNN+OOR1','KNN+OOR1+SF','KNN+OOR2','KNN+OOR2+SF'])
+plt.savefig("KNN_model_performance_mean_modified.png",dpi=200,bbox_inches = 'tight')
 plt.plot()
+
