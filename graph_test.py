5866d811fecf4d2f9776d9b1ecab3a7d3de0e0fb
paul028 <paulvincentnonat@gmail.com>
2021-02-18 12:31:05 +0800

    Revert "exp"
    This reverts commit 87c8722d34f5b6c8b31c12608e49f7ad3aebfa8b.

--- graph_test.py
+++ graph_test.py
@@ -23,36 +23,28 @@ def residuals(p, data):  # Residuals function needed by kmpfit
 
 #dataset 1 -128dB without SF, with SF
 x = np.array([1,2,3,4,5,6,7])
-dataset="modified"
-#trial_name1_mlp=["MLP_"+dataset+"/MLP+PCA=3.csv","MLP_"+dataset+"/MLP+PCA=5.csv","MLP_"+dataset+"/MLP+PCA=7.csv","MLP_"+dataset+"/MLP+PCA=10.csv","MLP_"+dataset+"/MLP+PCA=40.csv","MLP_"+dataset+"/MLP+PCA=44.csv","MLP_"+dataset+"/MLP.csv"]
-#trial_name1_ext=["extratress_"+dataset+"/ext+PCA=3.csv","extratress_"+dataset+"/ext+PCA=5.csv","extratress_"+dataset+"/ext+PCA=7.csv","extratress_"+dataset+"/ext+PCA=10.csv","extratress_"+dataset+"/ext+PCA=40.csv","extratress_"+dataset+"/ext+PCA=44.csv","extratress_"+dataset+"/ext.csv"]
-#trial_name1_knn=["KNN_"+dataset+"/KNN+PCA=3.csv","KNN_"+dataset+"/KNN+PCA=5.csv","KNN_"+dataset+"/KNN+PCA=7.csv","KNN_"+dataset+"/KNN+PCA=10.csv","KNN_"+dataset+"/KNN+PCA=40.csv","KNN_"+dataset+"/KNN+PCA=44.csv","KNN_"+dataset+"/KNN.csv"]
+#trial_name1=["MLP.csv","MLP+PCA=44.csv","MLP+PCA=40.csv","MLP+PCA=10.csv","MLP+PCA=7.csv","MLP+PCA=5.csv","MLP+PCA=3.csv"]
+#trial_name1=["ext+PCA=3.csv","ext+PCA=5.csv","ext+PCA=7.csv","ext+PCA=10.csv","ext+PCA=40.csv","ext+PCA=44.csv","ext.csv"]
+trial_name1=["KNN+PCA=3.csv","KNN+PCA=5.csv","KNN+PCA=7.csv","KNN+PCA=10.csv","KNN+PCA=40.csv","KNN+PCA=44.csv","KNN.csv"]
 
+trial_name2=["KNN+PCA=3+SF.csv","KNN+PCA=5+SF.csv","KNN+PCA=7+SF.csv","KNN+PCA=10+SF.csv","KNN+PCA=40+SF.csv","KNN+PCA=44+SF.csv","KNN+SF.csv"]
+#trial_name2=["MLP+SF.csv","MLP+PCA=44+SF.csv","MLP+PCA=40+SF.csv","MLP+PCA=10+SF.csv","MLP+PCA=7+SF.csv","MLP+PCA=5+SF.csv","MLP+PCA=3+SF.csv"]
+#trial_name2=["ext+SF.csv","ext+PCA=44+SF.csv","ext+PCA=40+SF.csv","ext+PCA=10+SF.csv","ext+PCA=7+SF.csv","ext+PCA=5+SF.csv","ext+PCA=3+SF.csv"]
 
-#trial_name2_mlp=["MLP_"+dataset+"/MLP+PCA=3+SF.csv","MLP_"+dataset+"/MLP+PCA=5+SF.csv","MLP_"+dataset+"/MLP+PCA=7+SF.csv","MLP_"+dataset+"/MLP+PCA=10+SF.csv","MLP_"+dataset+"/MLP+PCA=40+SF.csv","MLP_"+dataset+"/MLP+PCA=44+SF.csv","MLP_"+dataset+"/MLP+SF.csv"]
-#trial_name2_ext=["extratress_"+dataset+"/ext+PCA=3+SF.csv","extratress_"+dataset+"/ext+PCA=5+SF.csv","extratress_"+dataset+"/ext+PCA=7+SF.csv","extratress_"+dataset+"/ext+PCA=10+SF.csv","extratress_"+dataset+"/ext+PCA=40+SF.csv","extratress_"+dataset+"/ext+PCA=44+SF.csv","extratress_"+dataset+"/ext+SF.csv"]
-#trial_name2_knn=["KNN_"+dataset+"/KNN+PCA=3+SF.csv","KNN_"+dataset+"/KNN+PCA=5+SF.csv","KNN_"+dataset+"/KNN+PCA=7+SF.csv","KNN_"+dataset+"/KNN+PCA=10+SF.csv","KNN_"+dataset+"/KNN+PCA=40+SF.csv","KNN_"+dataset+"/KNN+PCA=44+SF.csv","KNN_"+dataset+"/KNN+SF.csv"]
+#dataset 2 Out of Range Dependent on SF
+#trial_name3=["MLP_SFD.csv","MLP+PCA=44_SFD.csv","MLP+PCA=40_SFD.csv","MLP+PCA=10_SFD.csv","MLP+PCA=7_SFD.csv","MLP+PCA=5_SFD.csv","MLP+PCA=3_SFD.csv"]
+#trial_name3=["ext_SFD.csv","ext+PCA=44_SFD.csv","ext+PCA=40_SFD.csv","ext+PCA=10_SFD.csv","ext+PCA=7_SFD.csv","ext+PCA=5_SFD.csv","ext+PCA=3_SFD.csv"]
+trial_name3=["KNN+PCA=3_SFD.csv","KNN+PCA=5_SFD.csv","KNN+PCA=7_SFD.csv","KNN+PCA=10_SFD.csv","KNN+PCA=40_SFD.csv","KNN+PCA=44_SFD.csv","KNN_SFD.csv"]
 
-##dataset 2 Out of Range Dependent on SF
-#trial_name3_mlp=["MLP_"+dataset+"/MLP+PCA=3_SFD.csv","MLP_"+dataset+"/MLP+PCA=5_SFD.csv","MLP_"+dataset+"/MLP+PCA=7_SFD.csv","MLP_"+dataset+"/MLP+PCA=10_SFD.csv","MLP_"+dataset+"/MLP+PCA=40_SFD.csv","MLP_"+dataset+"/MLP+PCA=44_SFD.csv","MLP_"+dataset+"/MLP_SFD.csv"]
-#trial_name3_ext=["extratress_"+dataset+"/ext+PCA=3_SFD.csv","extratress_"+dataset+"/ext+PCA=5_SFD.csv","extratress_"+dataset+"/ext+PCA=7_SFD.csv","extratress_"+dataset+"/ext+PCA=10_SFD.csv","extratress_"+dataset+"/ext+PCA=40_SFD.csv","extratress_"+dataset+"/ext+PCA=44_SFD.csv","extratress_"+dataset+"/ext_SFD.csv"]
-#trial_name3_knn=["KNN_"+dataset+"/KNN+PCA=3_SFD.csv","KNN_"+dataset+"/KNN+PCA=5_SFD.csv","KNN_"+dataset+"/KNN+PCA=7_SFD.csv","KNN_"+dataset+"/KNN+PCA=10_SFD.csv","KNN_"+dataset+"/KNN+PCA=40_SFD.csv","KNN_"+dataset+"/KNN+PCA=44_SFD.csv","KNN_"+dataset+"/KNN_SFD.csv"]
-
-#
-trial_name4_mlp=["MLP_"+dataset+"/MLP+PCA=3+SF_SFD.csv","MLP_"+dataset+"/MLP+PCA=5+SF_SFD.csv","MLP_"+dataset+"/MLP+PCA=7+SF_SFD.csv","MLP_"+dataset+"/MLP+PCA=10+SF_SFD.csv","MLP_"+dataset+"/MLP+PCA=40+SF_SFD.csv","MLP_"+dataset+"/MLP+PCA=44+SF_SFD.csv","MLP_"+dataset+"/MLP+SF_SFD.csv"]
-trial_name4_ext=["extratress_"+dataset+"/ext+PCA=3+SF_SFD.csv","extratress_"+dataset+"/ext+PCA=5+SF_SFD.csv","extratress_"+dataset+"/ext+PCA=7+SF_SFD.csv","extratress_"+dataset+"/ext+PCA=10+SF_SFD.csv","extratress_"+dataset+"/ext+PCA=40+SF_SFD.csv","extratress_"+dataset+"/ext+PCA=44+SF_SFD.csv","extratress_"+dataset+"/ext+SF_SFD.csv"]
-trial_name4_knn=["KNN_"+dataset+"/KNN+PCA=3+SF_SFD.csv","KNN_"+dataset+"/KNN+PCA=5+SF_SFD.csv","KNN_"+dataset+"/KNN+PCA=7+SF_SFD.csv","KNN_"+dataset+"/KNN+PCA=10+SF_SFD.csv","KNN_"+dataset+"/KNN+PCA=40+SF_SFD.csv","KNN_"+dataset+"/KNN+PCA=44+SF_SFD.csv","KNN_"+dataset+"/KNN+SF_SFD.csv"]
-
-#trial_name=[trial_name1_mlp,trial_name1_ext,trial_name1_knn]
-#trial_name=[trial_name2_mlp,trial_name2_ext,trial_name2_knn]
-#trial_name=[trial_name3_mlp,trial_name3_ext,trial_name3_knn]
-trial_name=[trial_name4_mlp,trial_name4_ext,trial_name4_knn]
-
+#trial_name4=["MLP+SF_SFD.csv","MLP+PCA=44+SF_SFD.csv","MLP+PCA=40+SF_SFD.csv","MLP+PCA=10+SF_SFD.csv","MLP+PCA=7+SF_SFD.csv","MLP+PCA=5+SF_SFD.csv","MLP+PCA=3+SF_SFD.csv"]
+#trial_name3=["ext_SFD.csv","ext+PCA=44_SFD.csv","ext+PCA=40_SFD.csv","ext+PCA=10_SFD.csv","ext+PCA=7_SFD.csv","ext+PCA=5_SFD.csv","ext+PCA=3_SFD.csv"]
+trial_name4=["KNN+PCA=3+SF_SFD.csv","KNN+PCA=5+SF_SFD.csv","KNN+PCA=7+SF_SFD.csv","KNN+PCA=10+SF_SFD.csv","KNN+PCA=40+SF_SFD.csv","KNN+PCA=44+SF_SFD.csv","KNN+SF_SFD.csv"]
+trial_name=[trial_name1,trial_name2,trial_name3,trial_name4]
 df_list=list()
 for w in range(len(trial_name)):
     df =list()
     for q in range(len(trial_name[w])):
-        df.append(pd.read_csv(trial_name[w][q],header=None).iloc[1:].values.astype('float64'))#.values.astype('float64')
+        df.append(pd.read_csv("KNN_modified/"+trial_name[w][q],header=None).iloc[1:].values.astype('float64'))#.values.astype('float64')
     df_list.append(df) #[without,with][trial]
 
 
@@ -66,7 +58,7 @@ for w in range(len(trial_name)):
             temp.append(df_list[w][z][q][1])
             
         distance_error.append(temp)
-    distance_error_list.append(distance_error)
+    distance_error_list.append(distance_errerror)
 
 #df=df.iloc[1:].values.astype('float64')
 errors_mean_list=list()
@@ -103,18 +95,8 @@ ax.set_xticks(x)
 ax.set_xticklabels(labels)
 ax.set_ylabel("Mean Positioning Error(m)")
 ax.set_xlabel("Number of Receiving Base Station")
-if dataset=="modified":
-    ax.set_title("Performance Comparison in Reduced Atwerp LoRaWAN Dataset")
-if dataset=="original":
-    ax.set_title("Performance Comparison in Atwerp LoRaWAN Dataset")
-#ax.legend(['MLP+OOR1','MLP+OOR1+SF','MLP+OOR2','MLP+OOR2+SF'])
-#ax.legend(['ext+OOR1','ext+OOR1+SF','ext+OOR2','ext+OOR2+SF'])
-#ax.legend(['KNN+OOR1','KNN+OOR1+SF','KNN+OOR2','KNN+OOR2+SF'])
-
-#ax.legend(['MLP+OOR1','ext+OOR1','KNN+OOR1'])
-#ax.legend(['MLP+OOR1+SF','ext+OOR1+SF','KNN+OOR1+SF'])
-#ax.legend(['MLP+OOR2','ext+OOR2','KNN+OOR2'])
-ax.legend(['MLP+OOR2+SF','ext+OOR2+SF','KNN+OOR2+SF'])
-plt.savefig("OOR2+SF_performance_mean_"+dataset+".png",dpi=200,bbox_inches = 'tight')
+ax.set_title("Performance Comparison in Reduced Atwerp LoRaWAN Dataset")
+ax.legend(['KNN+OOR1','KNN+OOR1+SF','KNN+OOR2','KNN+OOR2+SF'])
+plt.savefig("KNN_model_performance_mean_modified.png",dpi=200,bbox_inches = 'tight')
 plt.plot()
 
